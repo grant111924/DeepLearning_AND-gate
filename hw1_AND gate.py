a2a4713c2f7d7050a2efff4fe40dfd88013fe149
Grant <grant111924@gmail.com>
2017-10-04 15:25:10 +0800

    修改參數跟變數名稱
    eta 改成random
    檢查變數共用問題

--- hw1_AND gate.py
+++ hw1_AND gate.py
@@ -15,15 +15,21 @@ x=T.vector() #data feature value
 y=T.scalar() #data labeled value 0 or 1
 w=theano.shared(np.array([random.random(),random.random()],dtype=np.float64),'w')#weight inital value=[1,1]
 b=theano.shared(0.)  #bias inital value=0
-eta=0.5   #Learning rate
-time = 150000 #training_iterations times
-               
-z=T.dot(w,x)+b   #compute z=wX+b 
-sigmF=1/(1+T.exp(-z))  # sigmoid activation function => [0,1] interval value        
-cost =T.sum((sigmF-y)**2) #cost function  
+eta=random.random()  #Learning rate
+time = 30000 #training_iterations times
+
+def parameterWXB(weight,vector,bias):#comupte z=wX+b 
+    neuron=T.dot(weight,vector)+bias  
+    return neuron           
+def sigmoid(z):# sigmoid activation function => [0,1] interval value 
+    sigmo=1/(1+T.exp(-z)) 
+    return  sigmo    
+
+z=parameterWXB(w,x,b) #compute z 
+aF=sigmoid(z)  # sigmoid activation function
+cost =T.sum((aF-y)**2) #cost function  
 dw,db=T.grad(cost,[w,b]) #gradient compute
 gradient = theano.function([x,y],updates=[(w,w-eta*dw),(b,b-eta*db)])# update function
-neurnoPredict = theano.function([x],sigmF)  #computer predict result
 # Set inputs and correct output values
 X=[[0,0],[1,1],[1,0],[0,1]]
 Y=[0,1,0,0]
@@ -31,23 +37,21 @@ Y=[0,1,0,0]
 #Training data
 for t in range(time):
      i=random.randrange(0,4)
-     x=X[i]
-     y=Y[i]
-     #print(x,y)
-     #print (neurno(x))
-     gradient(x,y)
+     trainingX=X[i]
+     trainingY=Y[i]
+     gradient(trainingX,trainingY)
      #print(w.get_value(),b.get_value())
  
 #Get the best Weight and Bias
-W=w.get_value()
-B=b.get_value()
+bestW=w.get_value()
+bestB=b.get_value()
 dataX=[]
 dataY=[]
 
 #draw picture
 for i in range(1000):
     x_axis = random.uniform(0,1)
-    y_axis = (W[0]*x_axis+B)/(-W[1])
+    y_axis = (bestW[0]*x_axis+bestB)/(-bestW[1])
     dataX.append(x_axis)
     dataY.append(y_axis)
    # print(x_axis)
@@ -58,7 +62,14 @@ plt.ylim(0,1)
 plt.show() 
 
 #Testing data
-test=[[1,1],[1,0],[1,1],[0,1],[1,1]]
+test=[[1,1],[1,0],[1,1],[0,1],[1,1],[0,0]]
+z=T.dot(bestW,x)+bestB   #compute z=wX+b 
+sigmF=1/(1+T.exp(-z))  # sigmoid activation function => [0,1] interval value  
+neurnoPredict = theano.function([x],sigmF)  #computer predict result
+print("Eta(random):",eta)
+print("Iteration:",time)
+print("Best B:",bestB)
+print("Best W:",bestW)
+print("Testing......")
 for i in range(len(test)):
-    print(neurnoPredict(test[i]))
-
+    print("input:%r result: %f"%(test[i],neurnoPredict(test[i])))
